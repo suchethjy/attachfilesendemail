687a6bbdb20158b488bea390654c73321b64f27a
emailattachment <sjy@dminc.com>
2022-02-04 18:39:57 +0530

    Customextension-Backoffice

---
+++ practicebackoffice/gensrc/org/practicebackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jan 28, 2022, 12:37:42 PM                   ---
+ * ----------------------------------------------------------------
+ */
+package org.practicebackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "practicebackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
